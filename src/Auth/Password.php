c8f27b9e17ba366623d63b90c04029c2e110ba4a
Sgenmi <sgenmi@gmail.com>
2020-03-28 23:15:00 +0800

    add:password

--- src/Auth/Password.php
+++ src/Auth/Password.php
@@ -9,6 +9,21 @@ namespace Sgenmi\eYaf\Auth;
 
 class Password
 {
+    /**
+     * @param string $password
+     * @param string $hash
+     * @return bool
+     */
+    public static function verify(string $password, string $hash):bool {
+        return password_verify($password,$hash);
+    }
 
-
+    /**
+     * @desc 官方建议数据库字段255个字符
+     * @param string $password
+     * @return string
+     */
+    public static function hash(string $password):string {
+       return password_hash($password, PASSWORD_DEFAULT);
+    }
 }
