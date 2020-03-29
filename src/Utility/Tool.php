62393af68440438a8e63b07d194c44d3be260f19
Sgenmi <sgenmi@gmail.com>
2020-03-29 14:49:14 +0800

    增加常用方法

--- src/Utility/Tool.php
+++ src/Utility/Tool.php
@@ -10,10 +10,13 @@ namespace Sgenmi\eYaf\Utility;
 
 class Tool
 {
-    // 获取IP
-   public static function getClientIp()
+    /**
+     * @desc 获取客户端IP
+     * @return string
+     */
+    public static function getClientIp(): string
     {
-        $ser =(array) ((new \Yaf\Request\Http())->getServer());
+        $ser = (array)((new \Yaf\Request\Http())->getServer());
         if (isset($ser["HTTP_CLIENT_IP"])) {
             $ip = $ser["HTTP_CLIENT_IP"];
         } else if (isset($ser["HTTP_X_FORWARDED_FOR"])) {
@@ -24,6 +27,38 @@ class Tool
             $ip = "Unknow";
         }
         return $ip;
+    }
+
+    /**
+     * @param string $arg
+     * @return array|mixed
+     */
+    public static function getConfig(string $arg = '')
+    {
+        $_config = \Yaf\Registry::get('_config');
+        if (empty($arg)) {
+            return $_config;
+        } else {
+            return isset($_config->$arg) ? $_config->$arg : [];
+        }
+    }
+    /**
+     * @desc 分转元
+     * @param int $val
+     * @return float
+     */
+    public static function centToYuan(int $val): float
+    {
+        return sprintf("%.2f", $val / 100);
+    }
+    /**
+     * @desc 元转分
+     * @param float $val
+     * @return float|int
+     */
+    public static function fn_yuanToCent(float $val)
+    {
+        return $val * 100;
     }
 
 }
