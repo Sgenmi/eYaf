704e81387faae090d66efa097393e710e5e1cbd0
Sgenmi <sgenmi@gmail.com>
2023-07-15 18:47:56 +0800

    add:增加日志对外扩展类

---
+++ src/Logger/AbstractMonoLog.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Author: Sgenmi
+ * Date: 2023/7/15 6:12 PM
+ * Email: 150560159@qq.com
+ */
+
+namespace Sgenmi\eYaf\Logger;
+
+abstract class AbstractMonoLog extends MonoLog
+{
+
+
+}
