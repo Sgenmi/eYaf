c68984cb41895af4e3b30ec4fb140b0d2ab260b8
Sgenmi <sgenmi@gmail.com>
2023-11-17 15:53:23 +0800

    复写 selectContext，完成 INDEX,LOCK

--- src/Model/Medoo.php
+++ src/Model/Medoo.php
@@ -19,6 +19,98 @@ class Medoo extends catfanMedoo
     const LOCK_SHARE = ' LOCK IN SHARE MODE';
 
     /**
+     * @param string $table
+     * @param array $map
+     * @param $join
+     * @param $columns
+     * @param $where
+     * @param $columnFn
+     * @return string
+     * @author Sgenmi
+     */
+    protected function selectContext(string $table, array &$map, $join, &$columns = null, $where = null, $columnFn = null): string
+    {
+        preg_match('/(?<table>[\p{L}_][\p{L}\p{N}@$#\-_]*)\s*\((?<alias>[\p{L}_][\p{L}\p{N}@$#\-_]*)\)/u', $table, $tableMatch);
+
+        if (isset($tableMatch['table'], $tableMatch['alias'])) {
+            $table = $this->tableQuote($tableMatch['table']);
+            $tableAlias = $this->tableQuote($tableMatch['alias']);
+            $tableQuery = "{$table} AS {$tableAlias}";
+        } else {
+            $table = $this->tableQuote($table);
+            $tableQuery = $table;
+        }
+
+        $isJoin = $this->isJoin($join);
+
+        if ($isJoin) {
+            $tableQuery .= ' ' . $this->buildJoin($tableAlias ?? $table, $join, $map);
+        } else {
+            if (is_null($columns)) {
+                if (
+                    !is_null($where) ||
+                    (is_array($join) && isset($columnFn))
+                ) {
+                    $where = $join;
+                    $columns = null;
+                } else {
+                    $where = null;
+                    $columns = $join;
+                }
+            } else {
+                $where = $columns;
+                $columns = $join;
+            }
+        }
+
+        if (isset($columnFn)) {
+            if ($columnFn === 1) {
+                $column = '1';
+
+                if (is_null($where)) {
+                    $where = $columns;
+                }
+            } elseif ($raw = $this->buildRaw($columnFn, $map)) {
+                $column = $raw;
+            } else {
+                if (empty($columns) || $this->isRaw($columns)) {
+                    $columns = '*';
+                    $where = $join;
+                }
+
+                $column = $columnFn . '(' . $this->columnPush($columns, $map, true) . ')';
+            }
+        } else {
+            $column = $this->columnPush($columns, $map, true, $isJoin);
+        }
+        //force index
+        if(!empty($where['INDEX']) && $this->type=='mysql'){
+            if($this->isRaw($where['INDEX'])){
+                $forceIndex= $this->buildRaw($where['INDEX'],$map);
+            }else{
+                $forceIndex = $where['INDEX'];
+            }
+            if($isJoin){
+                $tableQuery = str_replace(sprintf('%s ',$table),sprintf('%s FORCE INDEX(`%s`) ',$table,$forceIndex),$tableQuery);
+            }else{
+                $tableQuery .=sprintf(' FORCE INDEX(%s)',$forceIndex);
+            }
+            unset($where['INDEX']);
+        }
+        //lock
+        $lock = $where['LOCK']??'';
+        if($lock){
+            unset($where['LOCK']);
+        }
+        $whereClause = $this->whereClause($where, $map);
+        if(!empty($lock) && $this->type=='mysql'){
+            $whereClause.= " ". $lock;
+        }
+        return 'SELECT ' . $column . ' FROM ' . $tableQuery . $whereClause;
+    }
+
+
+    /**
      * @param string $statement
      * @param array $map
      * @param callable|null $callback
